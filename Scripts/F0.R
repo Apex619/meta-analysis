bf5be140408ea8df9e36e3d99bef09b54ac3ecf7
Apex619 <z3375125@ad.unsw.edu.au>
2019-08-06 18:10:14 +1000

    Added code

--- Scripts/F0.R
+++ Scripts/F0.R
@@ -5,10 +5,12 @@ Body_Weight_F0 <- read.xlsx("C:\\Users\\hamanw\\Dropbox\\Meta Analysis Project\\
 
 library(metafor)
 #Calculate Effect Size (lnRR)
-Effect_Size_F0 <- escalc(measure="ROM", m1i=F0_Treatment_Mean, sd1i=F0_Treatment_SD, n1i=F0_Treatment_n, m2i=F0_Control_Mean, sd2i=F0_Control_SD, n2i=F0_Control_n, data=Body_Weight)
+Effect_Size_F0 <- escalc(measure="ROM", m1i=F0_Treatment_Mean, sd1i=F0_Treatment_SD, n1i=F0_Treatment_n, m2i=F0_Control_Mean, sd2i=F0_Control_SD, n2i=F0_Control_n, data=Body_Weight_F0)
 
 modelf0 <- rma.mv(yi, vi, random = ~ 1|Paper_ID, data=Effect_Size_F0)
 summary(modelf0)
+forest(modelf0)
+funnel(modelf0)
 
 modelf1 <- rma.mv(yi, vi, mod = ~Age_Days, random = ~ 1|Paper_ID, data=Effect_Size_F0)
 summary(modelf1)
