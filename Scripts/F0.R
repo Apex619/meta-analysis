0cb31fd23ca850f6254159639504aeab9c4698d8
Apex619 <z3375125@ad.unsw.edu.au>
2019-12-03 15:28:34 +1100

    Updating scripts and analyses, as well as plots and files

--- Scripts/F0.R
+++ Scripts/F0.R
@@ -5,6 +5,7 @@ Body_Weight_F0 <- read.xlsx("C:\\Users\\hamanw\\Dropbox\\Meta Analysis Project\\
 
 library(metafor)
 #Calculate Effect Size (lnRR)
+
 Effect_Size_F0 <- escalc(measure="ROM", m1i=F0_Treatment_Mean, sd1i=F0_Treatment_SD, n1i=F0_Treatment_n, m2i=F0_Control_Mean, sd2i=F0_Control_SD, n2i=F0_Control_n, data=Body_Weight_F0)
 
 modelf0 <- rma.mv(yi, vi, random = ~ 1|Paper_ID, data=Effect_Size_F0)
